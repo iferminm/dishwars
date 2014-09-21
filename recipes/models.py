328bdd1a0557c5e9ca929d986339e00c7d5bf580
Israel Fermín Montilla <iferminm@gmail.com>
2014-09-21 19:29:57 +0400

    First approach for the data model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=128)
+    
+
+class Step(models.Model):
+    action = models.TextField()
+    recipe = models.ForeignKey(Recipe)
+
+
+class Recipe(models.Model):
+    ingredients = models.ManyToManyField(Ingredient, through=RecipeIngredient)
+
+
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient)
+    recipe = models.ForeignKey(Recipe)
+    quantity = models.PositiveIntegerField()
+    unit = models.CharField(max_length=32)
+    
