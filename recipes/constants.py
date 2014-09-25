f7c28f65dc7b3850a3d791d8eec9b60932a8c781
Israel Fermín Montilla <iferminm@gmail.com>
2014-09-25 23:03:45 +0400

    Adds the constants needed for the recipes app

---
+++ recipes/constants.py
@@ -0,0 +1,19 @@
+TIME_UNIT_CHOICES = (
+    ('seconds', 'Seconds'),
+    ('minutes', 'Minutes'),
+    ('hours', 'Hours'),
+    ('days', 'Days'),
+    ('weeks', 'Weeks'),
+)
+
+
+INGREDIENT_UNIT_CHOICES = (
+    ('pinch', 'Pinch'),
+    ('teaspoon', 'Teaspoon'),
+    ('tablespoon', 'Tablespoon'),
+    ('cup', 'Cups'),
+    ('glass', 'Glass'),
+    ('litre', 'Litres'),
+    ('gram', 'Grams'),
+    ('kilo', 'Kilos'),
+)
