69ab47f0d2583d81a5156250910c4adb3de3db00
Israel Fermín Montilla <iferminm@gmail.com>
2014-09-25 23:15:58 +0400

    Registers existing models in admin site

--- dishes/admin.py
+++ dishes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Dish, Cuisine, MealType
+
+
+admin.site.register(Cuisine)
+admin.site.register(Dish)
+admin.site.register(MealType)
+
